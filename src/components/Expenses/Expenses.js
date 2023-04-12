c4473a8edc74664eab2ea91255099d578f8fade2
eliehannouch <elie.hannouche31@gmail.com>
2023-04-12 15:57:44 +0300

    inital init

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import Card from "../UI/Card";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear() === filteredYear;
+  });
+
+  return (
+    <>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        ></ExpensesFilter>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </>
+  );
+};
+
+export default Expenses;
